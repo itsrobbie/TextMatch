84ad48ad3d28ca921fabe1b4526b2d8f88683e87
Robert Burke <robertmurrayburke@gmail.com>
2015-12-10 12:33:17 +0000

    For some reason, the api ctrl wasn't in the api folder. Moved.

--- TextMatch.Mvc/Controllers/api/TextMatchController.cs
+++ TextMatch.Mvc/Controllers/api/TextMatchController.cs
@@ -13,6 +13,12 @@ namespace TextMatch.Mvc.Controllers
             _textMatcher = textMatcher;
         }
 
+        /// <summary>
+        /// Returns an int array of the possitions of all matches within the given string.
+        /// </summary>
+        /// <param name="text">The whole string to search though.</param>
+        /// <param name="subtext">The sub string to search for</param>
+        /// <returns></returns>
         [HttpGet]
         public IEnumerable<int> FindMatches(string text, string subtext)
         {
