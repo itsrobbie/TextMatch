cdb7acb73a9763d0fc29693b80f1c25155f9f596
Robert Burke <robertmurrayburke@gmail.com>
2016-11-30 00:53:49 -1100

    Adding a NUnit class to test the matcher, using the builder pattern.

---
+++ Tests/TextMatch.Logic.Tests/Builders/TextMatcherBuilder.cs
@@ -0,0 +1,12 @@
+﻿using TextMatch.Logic.Matchers;
+
+namespace TextMatch.Logic.Tests.Builders
+{
+    class TextMatcherBuilder
+    {
+        public TextMatcher Build()
+        {
+            return new TextMatcher();
+        }
+    }
+}
---
+++ Tests/TextMatch.Logic.Tests/TextMatcherNunit.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using TextMatch.Logic.Matchers;
+using TextMatch.Logic.Tests.Builders;
+
+namespace TextMatch.Logic.Tests
+{
+    [TestFixture]
+    public class TextMatcherNunit
+    {
+        private readonly TextMatcherBuilder matcherBuilder = new TextMatcherBuilder();
+
+        [TestCase("", "Test", 0)]
+        [TestCase("Test", "", 0)]
+        [TestCase("Test", "Fail", 0)]
+        [TestCase("Test", "Test", 1)]
+        [TestCase("TestTestTest", "Test", 3)]
+        [TestCase("TestTESTtest", "Test", 3)]
+        public void FindMatches_WithVariosInputs_ReturnsCorrectNumberOfResults(string text, string subtext, int matches)
+        {
+            //assign 
+            var matcher = this.matcherBuilder.Build();
+
+            //act
+            var result = matcher.FindMatches(text, subtext);
+
+            //assert
+            Assert.AreEqual(matches, result.Length);
+        }
+
+        [TestCase]
+        public void FindMatches_WithTextInput_ReturnsArrayWithCorrectPossitions()
+        {
+            //assign 
+            var matcher = this.matcherBuilder.Build();
+
+            //act
+            var result = matcher.FindMatches("Test TEST test", "test");
+
+            //assert
+            Assert.AreEqual(1, result[0]);
+            Assert.AreEqual(6, result[1]);
+            Assert.AreEqual(11, result[2]);
+        }
+    }
+}
